d0d13bfea4a8d508cbf0e3300e0c5c46aed02b44
karim-99-99 <kareem.hossam99@yahoo.com>
2026-01-22 01:11:33 +0200

    Add redirect from root to /api/

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -5,8 +5,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponseRedirect
+
+def redirect_to_api(request):
+    """Redirect root URL to /api/"""
+    return HttpResponseRedirect('/api/')
 
 urlpatterns = [
+    path('', redirect_to_api, name='root'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
